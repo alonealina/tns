449277abded8579943a7716bd8c2334cc28ea4ab
bluesky2008 <william_billl2008@yahoo.co.jp>
2022-02-14 20:21:09 +0900

    SERVICE,PRODUCTSページ　英語・韓国語・中国語版を作成

--- assets/js/inc.js
+++ assets/js/inc.js
@@ -2,7 +2,37 @@ $(function() {
   $('#header').load('inc/header.html', function () {
   });
 
-  $('#header_page').load('inc/header_page.html', function () {
+  $('#header_en').load('inc/header_en.html', function () {
+  });
+
+  $('#header_ko').load('inc/header_ko.html', function () {
+  });
+
+  $('#header_ch').load('inc/header_ch.html', function () {
+  });
+
+  $('#header_sv_ja').load('inc/header_sv_ja.html', function () {
+  });
+
+  $('#header_sv_en').load('inc/header_sv_en.html', function () {
+  });
+
+  $('#header_sv_ko').load('inc/header_sv_ko.html', function () {
+  });
+
+  $('#header_sv_ch').load('inc/header_sv_ch.html', function () {
+  });
+
+  $('#header_pr_ja').load('inc/header_pr_ja.html', function () {
+  });
+
+  $('#header_pr_en').load('inc/header_pr_en.html', function () {
+  });
+
+  $('#header_pr_ko').load('inc/header_pr_ko.html', function () {
+  });
+
+  $('#header_pr_ch').load('inc/header_pr_ch.html', function () {
   });
 
   $('#sidebar').load('inc/sidebar.html', function () {
